e95f4669d293718eec83dac3c831d5dec56104a5
Amarnath <motorolaonefusion@gmail.com>
2023-01-14 22:09:36 +0530

    Updation in Admin Ticket View

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from ticket import views as ticket
 
 router = routers.DefaultRouter()
 router.register(r'all-department', superuser.ViewAllDepartments, basename="view-departments")
+router.register(r'all-tickets', ticket.AdminTicketView, basename="admin-view-ticket")
 
 
 urlpatterns = [
@@ -14,6 +15,7 @@ urlpatterns = [
     path('delete-department/<str:id>/' , superuser.DeleteDepartment.as_view(), name='delete-department'),
     path('create-ticket/' , ticket.CreateTicketsView.as_view(), name='create-ticket'),
     path('update-ticket/<str:id>/' , ticket.UpdateTicketView.as_view(), name='update-ticket'),
+    path('user-view-ticket/<str:id>/' , ticket.UserTicketView.as_view(), name='view-ticket'),
 ]
 
 urlpatterns += router.urls
--- ticket/views.py
+++ ticket/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from .models import Tickets
 from .serializers import TicketSerializer
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet,ReadOnlyModelViewSet
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from accounts.models import Account
 
 # Create your views here.
+
+class AdminTicketView(ReadOnlyModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = Tickets.objects.all()
+    serializer_class = TicketSerializer
+
+
 
 class CreateTicketsView(APIView):
     permission_classes =[IsAuthenticated]
@@ -36,3 +45,18 @@ class UpdateTicketView(APIView):
             return Response({'Message' : 'Ticket Successfully Updated'}, status=status.HTTP_200_OK)
         else:
             return Response({'Message' : serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+
+class UserTicketView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request:Request,id):
+
+        user = Account.objects.get(id=id)
+        print(user.email)
+        email = user.email
+        try:
+            ticket = Tickets.objects.filter(email=email)
+        except:
+            return Response({'Message' : 'Data Not Found'} , status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = TicketSerializer(ticket , many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
